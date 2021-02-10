33d4f1365329804a9a534b73fc2891f61baf8cc3
johnnynode <johnnynode@gmail.com>
2021-02-10 11:31:45 +0800

    update: remove useless code

--- src/app.js
+++ src/app.js
@@ -14,16 +14,17 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'ejs'
+    extension: 'ejs'
 }))
 
+/*
 // logger
 app.use(async (ctx, next) => {
   const start = new Date()
@@ -31,6 +32,7 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+*/
 
 // routes
 app.use(index.routes(), index.allowedMethods())
@@ -38,7 +40,7 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 });
 
 module.exports = app
